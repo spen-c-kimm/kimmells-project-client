81adfa49eb340f8b952b1e5c3cb5aa0c49154cb4
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-26 22:23:45 -0500

    Created function to handle the inputs and btn. Createde function to handle signing up

--- src/Views/Signup.js
+++ src/Views/Signup.js
@@ -1,23 +1,59 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import { showSuccessAlert, showFailureAlert, showLoadingAlert } from "../utilities";
+import { withRouter } from "../withRouter";
 
-class Home extends Component {
+class Signup extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            message: "WELCOME!!!"
+            fullName: "",
+            userName: "",
+            password: "",
+            confirmPassword: "",
+            disabled: true
         };
-    }
+    };
 
-    componentWillMount() {
-        console.log("TEST")
-    }
+    handleSignupBtn() {
+        let disabled = false;
+
+        const fields = Object.keys(this.state).filter((e) => e !== "disabled");
+
+        for (const field of fields) {
+            if (!this.state[field] || this.state[field].length === 0) disabled = true;
+        }
+
+        this.setState({
+            disabled
+        });
+    };
+
+    eventHandler(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+
+        this.handleSignupBtn();
+    };
 
     async createUser() {
-        const res = await axios.post("http://localhost:3000/api/v1/signup");
-        console.log("res: ", res)
-    }
+        if (this.state.password !== this.state.confirmPassword) {
+            showFailureAlert("Passwords do not match.", "Warning");
+        } else {
+
+            const res = await showLoadingAlert("signup", { ...this.state });
+            const srvData = res?.data;
+
+            if (!srvData.success) {
+                await showFailureAlert(srvData.message);
+            } else {
+                localStorage.setItem("token", srvData.token);
+                await showSuccessAlert("New profile created successfully.");
+                this.props.navigate('/');
+            }
+        }
+    };
 
     render() {
         return (
@@ -25,11 +61,11 @@ class Home extends Component {
                 <div>
                     <h1>Title</h1>
                     <h2>Sub Title</h2>
-                    <input placeholder="Full Name"/>
-                    <input placeholder="Username"/>
-                    <input placeholder="Password"/>
-                    <input placeholder="Confirm Password"/>
-                    <button onClick={this.createUser}>Sign up</button>
+                    <input placeholder="Full Name" name="fullName" onChange={this.eventHandler.bind(this)} />
+                    <input placeholder="Username" name="userName" onChange={this.eventHandler.bind(this)} />
+                    <input placeholder="Password" name="password" onChange={this.eventHandler.bind(this)} type="password"/>
+                    <input placeholder="Confirm Password" name="confirmPassword" onChange={this.eventHandler.bind(this)} type="password"/>
+                    <button onClick={this.createUser.bind(this)} disabled={this.state.disabled}>Sign up</button>
                 </div>
                 <div>
                     <p>Have an account? <Link to={"/"}><span>Log in</span></Link></p>
@@ -37,6 +73,6 @@ class Home extends Component {
             </div>
         );
     }
-}
+};
 
-export default Home;
+export default withRouter(Signup);
