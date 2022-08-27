6c30be9831705b876cab327eb8931917a9bb0dbf
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-26 22:19:59 -0500

    Added call to validate token

--- src/api.js
+++ src/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
-export default async function api(route, params) {
+export default async function api(route, params, requiresAuth = false) {
     try {
+
+        if (requiresAuth) {
+            const token = localStorage.getItem("token");
+            const validation = await axios.post(`http://localhost:3000/api/v1/validateSession`, { token });
+            const validated = validation?.success;
+
+            if (!validated) {
+                return { validated };
+            };
+        };
+
         const res = await axios.post(`http://localhost:3000/api/v1/${route}`, params);
         return res;
     } catch (error) {
