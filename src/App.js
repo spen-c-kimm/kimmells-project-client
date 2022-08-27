7b771a944e4c403f1c7342b37c2b1bed206f1f7b
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-27 18:17:57 -0500

    Added Feed and Profile components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Views/Home"
-import Signup from "./Views/Signup"
+import Home from "./Views/Home";
+import Signup from "./Views/Signup";
+import Feed from "./Views/Feed";
+import Profile from "./Views/Profile";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/signup" exact element={<Signup />} />
+          <Route path="/feed" exact element={<Feed />} />
+          <Route path="/profile" exact element={<Profile />} />
         </Routes>
       </div>
     </Router>
