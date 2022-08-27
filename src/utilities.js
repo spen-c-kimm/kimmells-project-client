dfd91d92e32aa237f8fd20e5acd044a4a1ec6fd2
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-26 22:21:20 -0500

    Changed swal to swal.fire. Created function to show a loading alert while making an api call

--- src/utilities.js
+++ src/utilities.js
@@ -1,8 +1,13 @@
-import swal from 'sweetalert';
+import swal from 'sweetalert2';
+import api from "./api";
+
+export async function wait(timeToDelay) {
+    return new Promise((resolve) => setTimeout(resolve, timeToDelay));
+}
 
 export async function showSuccessAlert(message, title = "Success!", timer = 1500) {
     return new Promise((resolve) => {
-        swal({
+        swal.fire({
             title: title,
             text: message,
             icon: "success",
@@ -16,7 +21,7 @@ export async function showSuccessAlert(message, title = "Success!", timer = 1500
 
 export async function showFailureAlert(message, title = "Error!", timer = 1500) {
     return new Promise((resolve) => {
-        swal({
+        swal.fire({
             title: title,
             text: message,
             icon: "error",
@@ -26,5 +31,34 @@ export async function showFailureAlert(message, title = "Error!", timer = 1500) 
             resolve();
         });
 
+    });
+}
+
+export async function showLoadingAlert(action, params, requiresAuth = false) {
+    return new Promise((resolve) => {
+        try {
+            swal.fire({
+                title: "Loading...",
+                timerProgressBar: true,
+                allowOutsideClick: () => !swal.isLoading(),
+                didOpen: async () => {
+                    let time = new Date().getTime();
+                    swal.showLoading();
+
+                    const res = await api(action, params, requiresAuth)
+
+                    time = new Date().getTime() - time;
+
+                    if (time < 1000) {
+                        await wait(1000 - time);
+                    }
+
+                    swal.close();
+                    resolve(res);
+                }
+            });
+        } catch (error) {
+            resolve(error);
+        }
     });
 }
