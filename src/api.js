16bc4c6e05b8db3f89ffd06425eba27de3ab8e8b
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-27 18:17:26 -0500

    Updated api.js

--- src/api.js
+++ src/api.js
@@ -6,14 +6,19 @@ export default async function api(route, params, requiresAuth = false) {
         if (requiresAuth) {
             const token = localStorage.getItem("token");
             const validation = await axios.post(`http://localhost:3000/api/v1/validateSession`, { token });
-            const validated = validation?.success;
+            const srvData = validation?.data;
 
-            if (!validated) {
-                return { validated };
+            if (!srvData.success) {
+                return { authenticated: false };
             };
         };
 
         const res = await axios.post(`http://localhost:3000/api/v1/${route}`, params);
+
+        if (res) {
+            res.authenticated = true
+        }
+
         return res;
     } catch (error) {
         return error;
