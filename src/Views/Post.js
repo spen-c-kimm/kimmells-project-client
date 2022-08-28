b6af4900ecda1bd9954833460506b90e9141e848
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-28 14:41:29 -0500

    Added styles to the post component

--- src/Views/Post.js
+++ src/Views/Post.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+const image = require("../assets/default-profile-picture.PNG")
+import api from "../api";
 
 class Post extends Component {
     constructor(props) {
@@ -13,21 +15,26 @@ class Post extends Component {
         console.log("this.props.obj: ", this.props.obj)
     }
 
+    async likePost(postID) {
+        const res = await api("likePost", { userID: localStorage.getItem("userID"), postID }, true);
+        console.log("res: ", res);
+    }
+
     render() {
         return (
-            <div>
-                <div>
-                    <img></img>
+            <div className="post">
+                <div className="row post-banner">
+                    <img className="post-profile-picture" src={image}/>
                     {this.props.obj.useLinks ?
                         <Link to={{ pathname: `/profile/${this.props.obj.userID}` }}><p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p></Link>
                         : <p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p>
                     }
 
                 </div>
-                <div>
+                <div className="post-text">
                     <p>{this.props.obj.text}</p>
                 </div>
-                <div>
+                <div className="post-actions">
                     <a>Replies</a>
                     <a>Reply</a>
                     <a>Like</a>
