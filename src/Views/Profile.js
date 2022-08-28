851be146def6b88641fbd9738b92a42313733b17
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-28 14:39:13 -0500

    Added styles to the profile page

--- src/Views/Profile.js
+++ src/Views/Profile.js
@@ -4,6 +4,7 @@ import { showLoadingAlert, showFailureAlert } from "../utilities";
 import { withRouter } from "../withHooks";
 import Post from "./Post";
 import Footer from "./Footer";
+const image = require("../assets/default-profile-picture.PNG")
 
 class Profile extends Component {
     constructor(props) {
@@ -35,22 +36,29 @@ class Profile extends Component {
 
     render() {
         return (
-            <div>
-                <div>
-                    <input placeholder="Search" />
+            <div className="background">
+                <div className="page">
+                    <div className="profile-top">
+                        <div>
+                            <div className="banner"><img className="profile-picture" src={image} /></div>
+                            <div className="row">
+                                <strong><p>{this.state.user.fullName}</p></strong>
+                                <p>@{this.state.user.userName}</p>
+                            </div>
+                            <p>{this.state.user.bio}</p>
+                        </div>
+                        <div className="centered">
+                            <button>Posts</button>
+                            <button>Likes</button>
+                        </div>
+                    </div>
+                    <div className="posts-container">
+                        {this.state.posts.map(p => {
+                            return <Post obj={p} />
+                        })}
+                    </div>
+                    <Footer />
                 </div>
-                <div>
-                    <img />
-                    <h3>{this.state.user.fullName}</h3>
-                    <p>@{this.state.user.userName}</p>
-                    <p>{this.state.user.bio}</p>
-                </div>
-                <div>
-                    {this.state.posts.map(p => {
-                        return <Post obj={p} />
-                    })}
-                </div>
-                <Footer />
             </div>
         );
     }
