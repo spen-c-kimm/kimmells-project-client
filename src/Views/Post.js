06299f6e2f710e01abc1464812f44c78a4d7be1b
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-28 09:14:54 -0500

    Update Post.js

--- src/Views/Post.js
+++ src/Views/Post.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-import { showFailureAlert, showLoadingAlert, showPostAlert } from "../utilities";
-import { withRouter } from "../withRouter";
+import { Link } from "react-router-dom";
 
 class Post extends Component {
     constructor(props) {
@@ -11,23 +9,32 @@ class Post extends Component {
         };
     }
 
+    componentDidMount() {
+        console.log("this.props.obj: ", this.props.obj)
+    }
+
     render() {
         return (
             <div>
                 <div>
                     <img></img>
-                    <p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p>
+                    {this.props.obj.useLinks ?
+                        <Link to={{ pathname: `/profile/${this.props.obj.userID}` }}><p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p></Link>
+                        : <p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p>
+                    }
+
                 </div>
                 <div>
                     <p>{this.props.obj.text}</p>
                 </div>
                 <div>
-                    <a>Reply</a>                
-                    <a>Like</a>                
+                    <a>Replies</a>
+                    <a>Reply</a>
+                    <a>Like</a>
                 </div>
             </div>
         );
     }
 }
 
-export default withRouter(Post);
+export default Post;
