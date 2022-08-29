f8d9c49924a944bbd23c1a801e98e81e33de8386
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-28 19:32:15 -0500

    Updated Post.js

--- src/Views/Post.js
+++ src/Views/Post.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-const image = require("../assets/default-profile-picture.PNG")
 import api from "../api";
+const profile = require("../assets/default-profile-picture.PNG");
+const like = require("../assets/like-icon.png");
+const liked = require("../assets/liked-icon.png");
 
 class Post extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            liked: props.obj.liked === 1
         };
     }
 
-    componentDidMount() {
-        console.log("this.props.obj: ", this.props.obj)
-    }
+    // componentDidMount() {
+    //     console.log("this.props.obj: ", this.props.obj)
+    //     console.log("this.state.liked: ", this.state.liked)
+    // }
 
-    async likePost(postID) {
+    async likePost() {
+        const liked = this.state.liked;
+        this.setState({ liked: !liked });
+        const postID = this.props.obj.postID;
         const res = await api("likePost", { userID: localStorage.getItem("userID"), postID }, true);
         console.log("res: ", res);
     }
@@ -24,7 +30,7 @@ class Post extends Component {
         return (
             <div className="post">
                 <div className="row post-banner">
-                    <img className="post-profile-picture" src={image}/>
+                    <img className="post-profile-picture" src={profile}/>
                     {this.props.obj.useLinks ?
                         <Link to={{ pathname: `/profile/${this.props.obj.userID}` }}><p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p></Link>
                         : <p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p>
@@ -37,7 +43,7 @@ class Post extends Component {
                 <div className="post-actions">
                     <a>Replies</a>
                     <a>Reply</a>
-                    <a>Like</a>
+                    <img src={ this.state.liked ? liked : like } className="likeBtn" onClick={this.likePost.bind(this)}/>
                 </div>
             </div>
         );
