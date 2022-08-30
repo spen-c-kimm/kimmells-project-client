37b4a985b81b55b22905a151ca834b76b143aafc
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-30 10:51:02 -0500

    Added useLocation to the hooks

--- src/withHooks.js
+++ src/withHooks.js
@@ -1,14 +1,16 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
 
 export const withRouter = (Component) => {
   const Wrapper = (props) => {
     const navigate = useNavigate();
     const params = useParams();
+    const location = useLocation();
     
     return (
       <Component
         navigate={navigate}
         params={params}
+        location={location}
         {...props}
         />
     );
