0cad492b9e328bd704c38e7d4bd3d1b56bd8746e
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-30 10:44:32 -0500

    Applied more styling. Added a callback for the replies page

--- src/Views/Post.js
+++ src/Views/Post.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import api from "../api";
-import { showReplyAlert } from "../utilities";
+import { showReplyAlert, formatDate } from "../utilities";
 const profile = require("../assets/default-profile-picture.PNG");
 const like = require("../assets/like-icon.png");
 const liked = require("../assets/liked-icon.png");
@@ -17,7 +17,7 @@ class Post extends Component {
     }
 
     async handleReplyBTn() {
-        await showReplyAlert(this.props.obj.userName, this.props.obj.postID);
+        await showReplyAlert(this.props.obj.userName, this.props.obj.postID, this.props.initPage);
     }
 
     async likePost() {
@@ -27,24 +27,35 @@ class Post extends Component {
         await api("likePost", { userID: localStorage.getItem("userID"), postID }, true);
     }
 
+    async handleRepliesCallback() {
+        const currentRoute = this.props.location?.pathname || null;
+
+        if (currentRoute && currentRoute.includes("replies")) {
+            this.props.initPage();
+        }
+    }
+
     render() {
         return (
             <div className="post">
                 <div className="row post-banner">
-                    <img className="post-profile-picture" src={profile}/>
-                    {this.props.obj.useLinks ?
-                        <Link to={{ pathname: `/profile/${this.props.obj.userID}` }}><p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p></Link>
-                        : <p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p>
-                    }
+                    <img className="post-profile-picture" src={profile} />
+                    <div>
+                        {this.props.obj.useLinks ?
+                            <Link to={{ pathname: `/profile/${this.props.obj.userID}` }}><p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p></Link>
+                            : <p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p>
+                        }
+                        <p>{this.props.obj.repliedToUserName ? <Link to={{ pathname: `/replies/${this.props.obj.repliedToPostID}` }}><span>Replying to @{this.props.obj.repliedToUserName}<br /></span></Link> : ""}{this.props.obj.text}</p>
+                    </div>
 
                 </div>
-                <div className="post-text">
-                    <p>{this.props.obj.text}</p>
-                </div>
-                <div className="post-actions">
-                    <Link to={{ pathname: `/replies/${this.props.obj.postID}` }}><img src={ replies } className="likeBtn"/></Link>
-                    <img src={ reply } className="likeBtn" onClick={ this.handleReplyBTn.bind(this) }/>
-                    <img src={ this.state.liked ? liked : like } className="likeBtn" onClick={this.likePost.bind(this)}/>
+                <div className="row post-bottom">
+                    <div className="post-actions">
+                        <Link onClick={this.handleRepliesCallback.bind(this)} to={{ pathname: `/replies/${this.props.obj.postID}` }}><img src={replies} className="likeBtn" /></Link>
+                        <img src={reply} className="likeBtn" onClick={this.handleReplyBTn.bind(this)} />
+                        <img src={this.state.liked ? liked : like} className="likeBtn" onClick={this.likePost.bind(this)} />
+                    </div>
+                    <p>{formatDate(this.props.obj.dateCreated)}</p>
                 </div>
             </div>
         );
