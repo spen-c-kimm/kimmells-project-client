b2c2d8643f8c9788488dbf376db3b78f14d3090e
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-30 10:50:45 -0500

    Added a follow user function to the component

--- src/Views/User.js
+++ src/Views/User.js
@@ -7,21 +7,35 @@ class User extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            following: props.obj?.following
         };
     }
 
-    componentDidMount() {
-        console.log("this.props.obj: ", this.props.obj)
-        console.log("this.state.liked: ", this.state.liked)
+    async followUser() {
+        let following = this.state.following;
+        const followerID = localStorage.getItem("userID");
+        const followingID = this.props.obj.ID;
+
+        if (following === 0) {
+            following = 1;
+        } else {
+            following = 0;
+        }
+
+        await api("followUser", { followerID, followingID }, true);
+
+        this.setState({ following });
     }
 
     render() {
         return (
             <div className="post">
                 <div className="row">
-                <img className="post-profile-picture" src={profile}/>
-                <Link to={{ pathname: `/profile/${this.props.obj.ID}` }} ><p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p></Link>
+                    <div className="profile-banner row">
+                        <img className="post-profile-picture" src={profile} />
+                        <Link to={{ pathname: `/profile/${this.props.obj.ID}` }} ><p><strong>{this.props.obj.fullName}</strong> @{this.props.obj.userName}</p></Link>
+                    </div>
+                    <div style={{ display: "flex", height: "5vh", alignItems: "center" }}><button className={`followBtn userFollow ${this.state.following === 1 ? "blackBtn" : ""}`} onClick={this.followUser.bind(this)}>{`${this.state.following === 1 ? "Following" : "Follow"}`}</button></div>
                 </div>
             </div>
         );
